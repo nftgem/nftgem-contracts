01335fb1bed7ec2b22b0549fc3025c415f1958c5
cryptoleadfoot <cryptoleadfoot108@protonmail.com>
2021-04-20 20:45:18 -0700

    intial commit

--- nftgem-ui/src/assets/js/bs-init.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-	var hoverAnimationTriggerList = [].slice.call(document.querySelectorAll('[data-bss-hover-animate]'));
-	var hoverAnimationList = hoverAnimationTriggerList.forEach(function (hoverAnimationEl) {
-		hoverAnimationEl.addEventListener('mouseenter', function(e){ e.target.classList.add('animated', e.target.dataset.bssHoverAnimate) });
-		hoverAnimationEl.addEventListener('mouseleave', function(e){ e.target.classList.remove('animated', e.target.dataset.bssHoverAnimate) });
-	});
-}, false);
