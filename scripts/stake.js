5d145b5e76f05433d4f56ec67a1acb8452fe1b80
cryptoleadfoot <cryptoleadfoot108@protonmail.com>
2021-04-27 20:55:32 -0700

    updated

--- scripts/stake.js
+++ scripts/stake.js
@@ -12,15 +12,25 @@ async function main() {
   const atoken = ethers.utils.getAddress(
     '0x496FEC70974870dD7c2905E25cAd4BDE802938C7'
   );
+  const aFeeManager = ethers.utils.getAddress(
+    '0x70EC520bC874750815a1CD5109F6dF9A971AcF2A'
+  );
+  const aGemPoolFactory = ethers.utils.getAddress(
+    '0x70EC520bC874750815a1CD5109F6dF9A971AcF2A'
+  );
 
   const myAddress = await sender.getAddress();
   console.log(myAddress);
 
+  const GemPoolFactory = await ethers.getContractFactory('NFTGemPoolFactory');
+  const FeeManager = await ethers.getContractFactory('NFTGemFeeManager');
   const Token = await ethers.getContractFactory('NFTGemMultiToken');
   const Pool = await ethers.getContractFactory('NFTGemPool');
   const Data = await ethers.getContractFactory('NFTGemPoolData');
 
   const token = await Token.attach(atoken);
+  const gemPoolFactory = await GemPoolFactory.attach(aGemPoolFactory);
+
   const pool = await Pool.attach(stick);
   const data = await Data.attach(stick);
   const min = await data.minTime();
@@ -29,19 +39,24 @@ async function main() {
     const claimHash = await pool.nextClaimHash();
     const value = await data.ethPrice();
     const abal = await token.balanceOf(myAddress, BigNumber.from(0));
-    await pool.createClaims(min, 1, {value, gasLimit: 4200000});
+    await pool.createClaims(min.add(10), 1, {
+      value: value.add(100),
+      gasLimit: 4200000,
+    });
     console.log(`${abal} stick ${claimHash} purchased for ${value}`);
-    setTimeout(
-      () =>
+    setTimeout(() => {
+      try {
         pool
           .collectClaim(claimHash, {gasLimit: 4200000})
-          .then(() => console.log(`claim ${claimHash} collected`)),
-      35000
-    );
-    setTimeout(() => stakeStick(), 5000);
+          .then(() => console.log(`claim ${claimHash} collected`));
+      } catch (e) {
+        /** */
+      }
+    }, 35000);
   }
 
-  stakeStick();
+  //collectClaims().then(() => {});
+  setInterval(() => stakeStick(), 8000);
 
   stall();
 }
