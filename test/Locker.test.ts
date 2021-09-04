e06695daaeba3da7742348f2ffff95db177ac0ad
firllyw <fwahyudi17@gmail.com>
2021-09-04 21:07:06 +0700

    feat: pickuptoken test

--- test/Locker.test.ts
+++ test/Locker.test.ts
@@ -7,31 +7,45 @@ import {
 } from './fixtures/Governance.fixture';
 
 describe('Locker Test Suite', function () {
-  let Locker: Contract;
+  let locker: Contract;
   let NFTGemMultiToken: any;
   const tokenHash = keccak256(['bytes'], [pack(['string'], ['Test Token'])]);
   beforeEach(async () => {
     // Deploy Locker Contract on each test iteration
-    Locker = await (await ethers.getContractFactory('Locker')).deploy();
+    locker = await (await ethers.getContractFactory('Locker')).deploy();
     const setupNftGemGovernorResult = await setupNftGemGovernor();
     NFTGemMultiToken = setupNftGemGovernorResult.NFTGemMultiToken;
     const [sender] = await ethers.getSigners();
     // Mint token to sender to make sure the sender have enough balance
     await NFTGemMultiToken.mint(sender.address, tokenHash, 100);
-    await NFTGemMultiToken.connect(sender).setApprovalForAll(Locker.address, true);
+    await NFTGemMultiToken.connect(sender).setApprovalForAll(locker.address, true);
   })
   it('Drop Off', async function () {
-    await Locker.dropOff(
+    await locker.dropOff(
       NFTGemMultiToken.address,
       tokenHash,
       NFTGemMultiToken.address,
       tokenHash,
       5);
-    const LockerContent = await Locker.contents(tokenHash);
-    expect(LockerContent).to.not.be.undefined;
-    expect(LockerContent.awardTokenAddress).to.be.equal(NFTGemMultiToken.address);
-    expect(LockerContent.unlockTokenAddress).to.be.equal(NFTGemMultiToken.address);
-    expect(LockerContent.awardTokenHash).to.be.equal(tokenHash);
-    expect(LockerContent.unlockTokenHash).to.be.equal(tokenHash);
+    const lockerContent = await locker.contents(tokenHash);
+    expect(lockerContent).to.not.be.undefined;
+    expect(lockerContent.awardTokenAddress).to.be.equal(NFTGemMultiToken.address);
+    expect(lockerContent.unlockTokenAddress).to.be.equal(NFTGemMultiToken.address);
+    expect(lockerContent.awardTokenHash).to.be.equal(tokenHash);
+    expect(lockerContent.unlockTokenHash).to.be.equal(tokenHash);
+  });
+
+  it('Pick Up', async function () {
+    console.log(await locker.contents(tokenHash));
+    await locker.dropOff(
+      NFTGemMultiToken.address,
+      tokenHash,
+      NFTGemMultiToken.address,
+      tokenHash,
+      5,
+    )
+    await locker.pickUpTokenWithKey(tokenHash);
+    const lockerContent = await locker.contents(tokenHash);
+    expect(lockerContent.awardQty).to.be.equal(0);
   });
 });
